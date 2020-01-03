a631cf47d318518235677775ca0e69ddfba60243
tianxu <443789143@qq.com>
2020-01-03 17:43:05 +0800

    update

--- src/components/sider/index.tsx
+++ src/components/sider/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import './index.scss'
 
 const Sider = () => {
   return (
     <div className='sider-content'>
       <div className='user-model'>
-        <img className='user-img' src='../../assets/img/user.jpg' alt='' />
+        <div className='user-img-box'>
+          <img src={require('../../assets/img/user.jpg')} alt='' />
+        </div>
         <div className='username'>萌大大</div>
-        <div className='introduce'></div>
+        <div className='introduce'>千里之行，始于足下</div>
       </div>
     </div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from '../views/home/index'
+import File from '../views/file/index'
 
 export default () => {
   return (
     <Switch>
+      <Redirect from='/' to='/home' />
       <Route path='/home' component={Home}></Route>
+      <Route path='/file' component={File}></Route>
     </Switch>
   )
 }
